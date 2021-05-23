3fa381691e6d1533dae07ba35d62f649b5ab54c3
Nick Edwin <nickedwin85@gmail.com>
2021-05-23 14:23:23 -0600

    Updated relationship validations

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,6 @@
 class Tag < ApplicationRecord
   validates_presence_of :name
+
+  has_many :taggings, dependent: :destroy
+  has_many :tasks, through: :taggings
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,9 @@ class Task < ApplicationRecord
   validates_presence_of :title,
                         :description,
                         :due_date
+
   has_many :user_tasks, dependent: :destroy
   has_many :users, through: :user_tasks
+  has_many :taggings, dependent: :destroy
+  has_many :tags, through: :taggings
 end
