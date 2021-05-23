6e9a659ca62690df89a911d40fa388faf6a0b8a2
Nick Edwin <nickedwin85@gmail.com>
2021-05-23 14:14:22 -0600

    Added User_Tasks joins table for many-many relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_23_182100) do
+ActiveRecord::Schema.define(version: 2021_05_23_182441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 2021_05_23_182100) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "user_tasks", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "task_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["task_id"], name: "index_user_tasks_on_task_id"
+    t.index ["user_id"], name: "index_user_tasks_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "job"
@@ -30,4 +39,6 @@ ActiveRecord::Schema.define(version: 2021_05_23_182100) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "user_tasks", "tasks"
+  add_foreign_key "user_tasks", "users"
 end
