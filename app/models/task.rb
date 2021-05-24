6bf3c0c1750d15e604b4a51a694a4ae34e2eaae8
Nick Edwin <nickedwin85@gmail.com>
2021-05-24 13:37:42 -0600

    Added method to add tags to tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,4 +7,16 @@ class Task < ApplicationRecord
   has_many :users, through: :user_tasks
   has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings
+
+  def tag_list
+    self.tags.collect do |tag|
+      tag.name
+    end.join(", ")
+  end
+
+  def tag_list=(tags_string)
+    tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
+    new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
+    self.tags = new_or_found_tags
+  end
 end
